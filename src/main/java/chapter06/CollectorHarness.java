7871f0504b4aafd6eb54e4675c537f85fb1c4df8
유도건 <dgyoo@nextree.io>
2021-08-17 22:47:29 +0900

    20210817

---
+++ src/main/java/chapter06/CollectorHarness.java
@@ -0,0 +1,27 @@
+package chapter06;
+
+import java.util.function.Consumer;
+
+public class CollectorHarness {
+    //
+    public static void main(String[] args) {
+        //
+        System.out.println("Partitioning1 done in: " + execute(PartitionPrimeNumbers::partitionPrimes));
+        System.out.println("Partitioning2 done in: " + execute(PartitionPrimeNumbers::partitionPrime));
+    }
+
+    private static long execute(Consumer<Integer> primePartitioner) {
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            primePartitioner.accept(1_000_000);
+            long duration = (System.nanoTime()-start) / 1_000_000;
+            if (duration <fastest) {
+                //
+                fastest = duration;
+            }
+            System.out.println("done in " + duration);
+        }
+        return fastest;
+    }
+}
--- src/main/java/chapter06/PartitionPrimeNumbers.java
+++ src/main/java/chapter06/PartitionPrimeNumbers.java
@@ -1,7 +1,8 @@
 package chapter06;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collector;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.*;
@@ -13,6 +14,7 @@ public class PartitionPrimeNumbers {
         System.out.println(isPrime(100));
         System.out.println(isPrime2(100));
         System.out.println(partitionPrimes(100));
+        System.out.println(partitionPrime(100));
     }
 
     private static boolean isPrime(int candidate) {
@@ -28,4 +30,70 @@ public class PartitionPrimeNumbers {
         //
         return IntStream.rangeClosed(2,n).boxed().collect(partitioningBy(candidate->isPrime2(candidate)));
     }
+
+    public static Map<Boolean,List<Integer>> partitionPrime(int n) {
+//        return IntStream.rangeClosed(2,n).boxed()
+//                .collect(partitioningBy(candidate-> isPrime(candidate)));
+        return IntStream.rangeClosed(2,n).boxed()
+                .collect(new PrimeNumbersCollectors());
+    }
+
+    public static boolean isPrime(List<Integer> primes, int candidate) {
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return primes.stream()
+                .takeWhile(i->i<=candidateRoot)
+                .noneMatch(i-> candidate % i ==0);
+//        return takeWhile(primes,i->i<=candidateRoot).stream().noneMatch(p->candidate%p==0);
+    }
+
+    public static <A> List <A> takeWhile(List<A> list, Predicate<A> p) {
+        int i = 0;
+        for (A item: list) {
+            if (!p.test(item)) {
+                return list.subList(0,i);
+            }
+            i++;
+        }
+        return list;
+    }
+
+    public static class PrimeNumbersCollectors implements Collector<Integer,Map<Boolean,List<Integer>>,Map<Boolean,List<Integer>>> {
+        //
+        @Override
+        public Supplier<Map<Boolean, List<Integer>>> supplier() {
+            return ()-> new HashMap<Boolean, List<Integer>>() {
+                {
+                    put(true,new ArrayList<>());
+                    put(false,new ArrayList<>());
+                }
+            };
+        }
+
+        @Override
+        public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
+            return (Map<Boolean,List<Integer>> acc,Integer candidate) -> {
+                acc.get(isPrime(acc.get(true),candidate)).add(candidate);
+            };
+        }
+
+        @Override
+        public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
+            return (Map<Boolean,List<Integer>> map1, Map<Boolean,List<Integer>> map2) -> {
+                map1.get(true).addAll(map2.get(true));
+                map1.get(false).addAll(map2.get(false));
+                return map1;
+            };
+        }
+
+        @Override
+        public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
+            return Function.identity();
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH));
+        }
+    }
+
 }
